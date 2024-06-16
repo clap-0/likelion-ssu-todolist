3413fc76f4a3f7f044b53c12d6fc6dff07628691
clap-0 <clap-0@naver.com>
2024-06-17 00:19:29 +0900

    feat: initial commit

---
+++ userApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+  """ User Model Definition """
+  
+  username = models.CharField(
+    max_length=150,
+    unique=True
+  )
+  password = models.CharField(
+    max_length=150
+  )
+
+  def __str__(self):
+    return self.username
